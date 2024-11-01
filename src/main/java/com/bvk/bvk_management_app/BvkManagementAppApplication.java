dd1caac9ca052b1cdcf408262f3935f4364cb175
noprizal <rizalnov667@gmail.com>
2024-11-02 01:03:40 +0700

    first init

---
+++ src/main/java/com/bvk/bvk_management_app/BvkManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.bvk.bvk_management_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BvkManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BvkManagementAppApplication.class, args);
+	}
+
+}
